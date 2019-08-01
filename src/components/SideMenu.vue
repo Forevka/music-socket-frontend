7bebc6c85200b040a15b44a1a856db7d2ea3fc68
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2019-08-01 14:11:25 +0300

    Dynamically change menu

---
+++ src/components/SideMenu.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="menu-menu">
+    <sidebar-menu :menu=menu />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideMenu',
+  computed: {
+    menu () {
+      console.log(this)
+      return this.$store.MenuStore.getters.getMenu
+    }
+  }
+}
+</script>
